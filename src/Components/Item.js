b382a69197d416501cbb8ff8889a0a31eac7ca54
ebro1230 <ebro1230@gmail.com>
2023-08-29 21:48:29 +0200

    added prices, ratings, add and minus button functions, quantities, total price, cart array, functional add to cart button

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,24 +1,83 @@
 import React, { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Rating from "react-rating";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as faStarF } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarE } from "@fortawesome/free-regular-svg-icons";
 
 const HomePage = (props) => {
   const cardImage = props.cardImage;
   const cardTitle = props.cardTitle;
+  const cardRating = Number(props.cardRating);
+  const cardPrice = props.cardPrice;
   const cardText = props.cardText;
-  const number = 0;
+  const cardId = props.cardId;
+  const cardQuantity = props.cardQuantity;
+  const totalPrice = (cardQuantity * cardPrice).toFixed(2);
 
   return (
     <Card>
       <Card.Img variant="top" src={cardImage} />
       <Card.Body>
         <Card.Title>{cardTitle}</Card.Title>
+        <div className="card-rating">
+          <Rating
+            emptySymbol={
+              <FontAwesomeIcon
+                icon={faStarE}
+                size="xl"
+                style={{ color: "#f7eb02" }}
+              />
+            }
+            fullSymbol={
+              <FontAwesomeIcon
+                icon={faStarF}
+                size="xl"
+                style={{ color: "#f7eb02" }}
+              />
+            }
+            placeholderSymbol={
+              <FontAwesomeIcon
+                icon={faStarF}
+                size="xl"
+                style={{ color: "#f7eb02" }}
+              />
+            }
+            intialRating={cardRating}
+            placeholderRating={cardRating}
+            fractions={10}
+            step={1}
+            readonly
+            quiet
+          />
+        </div>
+        <h3 className="card-price">${cardPrice}</h3>
         <Card.Text>{cardText}</Card.Text>
         <div className="quantity-div">
-          <Button>-</Button>
-          <p>Quantity: {number}</p>
-          <Button>+</Button>
+          <Button id={cardId} onClick={props.onMinus}>
+            -
+          </Button>
+          <p>Quantity: {cardQuantity}</p>
+          <Button id={cardId} onClick={props.onPlus}>
+            +
+          </Button>
         </div>
+        {cardQuantity > 0 ? (
+          <div className="addToCart-div">
+            <div>
+              <p>Total Price: ${totalPrice}</p>
+            </div>
+            <Button
+              id={cardId}
+              data-quantity={cardQuantity}
+              onClick={props.onAddToCart}
+            >
+              Add to Cart
+            </Button>
+          </div>
+        ) : null}
       </Card.Body>
     </Card>
   );
--- src/Views/Homepage.js
+++ src/Views/Homepage.js
@@ -5,22 +5,130 @@ import Row from "react-bootstrap/Row";
 import axios from "axios";
 
 import Item from "../Components/Item";
+import LoadingIndicator from "../Components/LoadingIndicator";
 import shopping_cart from "../Images/shopping-cart.png";
 
 const HomePage = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [cart, setCart] = useState([]);
 
-  console.log(items);
+  const handleOnPlus = (e) => {
+    e.preventDefault();
+    const increaseId = e.target.id;
+    setItems(
+      items.map((item) => {
+        if (Number(item.id) === Number(increaseId)) {
+          return (item = {
+            category: item.category,
+            description: item.description,
+            id: item.id,
+            image: item.image,
+            price: item.price,
+            rating: item.rating,
+            title: item.title,
+            quantity: item.quantity + 1,
+          });
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  const handleOnMinus = (e) => {
+    e.preventDefault();
+    const decreaseId = e.target.id;
+    setItems(
+      items.map((item) => {
+        if (Number(item.id) === Number(decreaseId)) {
+          return (item = {
+            category: item.category,
+            description: item.description,
+            id: item.id,
+            image: item.image,
+            price: item.price,
+            rating: item.rating,
+            title: item.title,
+            quantity: item.quantity === 0 ? 0 : item.quantity - 1,
+          });
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  const handleOnAddToCart = (e) => {
+    e.preventDefault();
+    const itemId = e.target.id;
+    const itemQuantity = Number(e.target.getAttribute("data-quantity"));
+    setItems(
+      items.map((item) => {
+        if (Number(item.id) === Number(itemId)) {
+          setCart(
+            cart.find((cartItem) => cartItem.id === item.id)
+              ? cart.map((cartItem) => {
+                  if (Number(cartItem.id) === Number(item.id)) {
+                    return (cartItem = {
+                      category: cartItem.category,
+                      description: cartItem.description,
+                      id: cartItem.id,
+                      image: cartItem.image,
+                      price: cartItem.price,
+                      rating: cartItem.rating,
+                      title: cartItem.title,
+                      quantity:
+                        Number(cartItem.quantity) + Number(itemQuantity),
+                    });
+                  } else {
+                    return cartItem;
+                  }
+                })
+              : [...cart, item]
+          );
+          return (item = {
+            category: item.category,
+            description: item.description,
+            id: item.id,
+            image: item.image,
+            price: item.price,
+            rating: item.rating,
+            title: item.title,
+            quantity: 0,
+          });
+        } else {
+          return item;
+        }
+      })
+    );
+  };
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get(`https://fakestoreapi.com/products`)
       .then((response) => {
-        setItems(response.data);
+        setItems(
+          response.data.map((item) => {
+            return (item = {
+              category: item.category,
+              description: item.description,
+              id: item.id,
+              image: item.image,
+              price: item.price.toFixed(2),
+              rating: item.rating,
+              title: item.title,
+              quantity: 0,
+            });
+          })
+        );
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []);
 
@@ -28,7 +136,7 @@ const HomePage = () => {
     <div className="items-container">
       {isLoading ? (
         <div>
-          <h1>LOADING...</h1>
+          <LoadingIndicator />
         </div>
       ) : items.length ? (
         <Row xs={1} md={2} lg={3} xl={4} className="g-4">
@@ -37,7 +145,14 @@ const HomePage = () => {
               <Item
                 cardImage={item.image}
                 cardTitle={item.title}
+                cardRating={item.rating.rate}
+                cardPrice={item.price}
                 cardText={item.description}
+                cardQuantity={item.quantity}
+                cardId={item.id}
+                onPlus={handleOnPlus}
+                onMinus={handleOnMinus}
+                onAddToCart={handleOnAddToCart}
               />
             </Col>
           ))}
